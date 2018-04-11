298cd6d3c97b5c3d589e78f1b454fc393f4763b3
Niels Wijers <nielsjlwijers@gmail.com>
2018-04-11 12:04:17 +0200

    Refactored initial code

---
+++ Dialogs/krijgAdviesDialog.js
@@ -0,0 +1,13 @@
+const builder = require('botbuilder');
+
+module.exports = {
+    intentName: "KrijgAdvies",
+    dialogSteps: [
+        (session,args) => { 
+            var pijnpunt = builder.EntityRecognizer.findEntity(args.entities,'PijnPunt'); 
+            if (pijnpunt){ 
+              builder.Prompts.text(session, "Neem een Asperine"); 
+            } 
+        }, 
+    ]
+}
--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-var builder = require('botbuilder');
-var apiairecognizer = require('api-ai-recognizer');
 var express = require('express');
-var app = express();
+var Bot = require('./botApp.js');
+var server = express();
 
-app.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('listening');
+const bot = new Bot.Bot();
+bot.init();
+
+server.listen(3978, function () {
+  console.log('listening to port 3978');
 });
 
-var connector = new builder.ChatConnector({
-  appId: process.env.MicrosoftAppId,
-  appPassword: process.env.MicrosoftAppPassword
-});
-      
-var bot = new builder.UniversalBot(connector);
-      
-var recognizer = new apiairecognizer("54ae2c103dcd42b5b65c4d4cd120ed25");
-var intents = new builder.IntentDialog({
-  recognizers: [recognizer]
-});
+server.post('/api/messages', bot.getConnector().listen());
 
-app.post('/api/messages', connector.listen());
-
-bot.dialog('/',intents);
-
-intents.matches('KrijgAdvies',[
-  function(session,args){
-    var pijnpunt = builder.EntityRecognizer.findEntity(args.entities,'PijnPunt');
-    if (pijnpunt){
-      builder.Prompts.text(session, "Neem een Asperine");
-    }
-  }
-]);
-  
-intents.onDefault(function(session){
-  session.send("Sorry...can you please rephrase?");
-});
