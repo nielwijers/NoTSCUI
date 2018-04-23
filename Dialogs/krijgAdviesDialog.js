d6c7c5251d3bbdecdb878b0a452e423a4c8420e7
Jeroen Spans <jeroen.spans@elastique.nl>
2018-04-23 11:30:53 +0200

    + else statement added for pijnpunt.

--- Dialogs/krijgAdviesDialog.js
+++ Dialogs/krijgAdviesDialog.js
@@ -7,6 +7,8 @@ module.exports = {
             var pijnpunt = builder.EntityRecognizer.findEntity(args.entities,'PijnPunt');
             if (pijnpunt){ 
               builder.Prompts.text(session, "Neem een Aspirine");
+            } else {
+                builder.Prompts.text(session, "Ik kan je hierover geen advies geven.");
             }
         },
     ]
